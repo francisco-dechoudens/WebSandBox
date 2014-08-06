9fa3df8adf790cc0b1915082ecaf917e5fa82466
francisco-dechoudens <francisco.dechoudens@gmail.com>
2014-08-06 17:16:33 -0400

    Fix a lot of issues

--- parse-js-blank/js/KML2DKML.js
+++ parse-js-blank/js/KML2DKML.js
@@ -1,3 +1,6 @@
+var tab = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
+var tabCoord = +tab+tab+tab+tab+tab+tab+tab+tab+tab+tab+'&nbsp;';
+
 function readText(filePath,numberOfDivision) { 
 
 /*
@@ -24,10 +27,11 @@ function readText(filePath,numberOfDivision) {
                  data["File_Content" + j] = btoa(evt.target.result);
                  j++;
                  if (j == k){
-                 	 var dkmlConcat = "";
+                 	 var dkmlConcat = "<p style='margin-left:-30em;'>"+"&lt;Document>"+ "<br>";
                  	for(dkml in dkmlArray){
                  		dkmlConcat = dkmlConcat + dkmlArray[dkml];
                  	}
+                 	dkmlConcat = dkmlConcat+"&lt;/Document>";
                  	displayContents(dkmlConcat);
                  	alert("All file has been read");
                      
@@ -55,11 +59,18 @@ function ConvertKMLContent(txt,numberOfDivision) {
 		  var coordString = xmlDoc.getElementsByTagName("coordinates")[0].childNodes[0].nodeValue;
 		  var arrayOfCoord = coordString.split(" ");
 		  
-		  
+		  var arrayCleaner = new Array();
+		  for(var coord in arrayOfCoord){
+			if (arrayOfCoord[coord]) {
+				arrayCleaner.push(arrayOfCoord[coord]);
+			};
+		  }
+		  arrayOfCoord = arrayCleaner;
 		 
 		  var vertexesCoord = new Array();
 		  var vertexesId = new Array();
 
+		  var someIntFirstInt = someInt;
 		  for(var coord in arrayOfCoord){
 		  	if (!(someInt % numberOfDivision)) {
 
@@ -68,47 +79,68 @@ function ConvertKMLContent(txt,numberOfDivision) {
 		  	};
 		  	someInt = someInt + 1;
 		  }
+		  
+		  var lastElement = arrayOfCoord.pop();
+		  arrayOfCoord.push(lastElement);
+
+		  var firstElement = arrayOfCoord[0];
+		  if (vertexesCoord.indexOf(firstElement) == -1 ) {
+		  	vertexesId.unshift(someIntFirstInt);
+		  	vertexesCoord.unshift(firstElement);
+		  };
+		  if (vertexesCoord.indexOf(lastElement) == -1 ) {
+		  	vertexesId.push(someInt-1);
+		  	vertexesCoord.push(lastElement);
+		  };
+
 		  //vertexesId.push(someInt - 1)
 		  //vertexesCoord.push(arrayOfCoord[someInt-1]);//to add the last coord
 
-		  var tab = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
-		  var tabCoord = +tab+tab+tab+tab+tab+tab+tab+tab+tab+tab+'&nbsp';
-		  var dkml = "<p style='margin-left:-30em;'>"+"&lt;Document>"+ "<br>" +tab+ "&lt;Vertexes>" + "<br>";
+		  
+		  var dkml = tab+ "&lt;Vertexes>" + "<br>";
 		  for(var vertex in vertexesCoord){
 		  	dkml = dkml +tab+tab+ "&lt;vertex>"+ "<br>";
-		  	dkml = dkml +tab+tab+tab+ "&lt;id>"+ vertexesId[vertex] +"&lt;/id>"+ "<br>"
-		  	dkml = dkml +tabCoord+"&lt;coordinates>"+ vertexesCoord[vertex] +"&lt;/coordinates>"+ "<br>"
+		  	dkml = dkml +tab+tab+tab+ "&lt;id>"+ vertexesId[vertex] +"&lt;/id>"+ "<br>";
+		  	dkml = dkml +tab+tab+tab+ "&lt;coordinates>"+ vertexesCoord[vertex] +"&lt;/coordinates>"+ "<br>";
+		  	//dkml = dkml +tabCoord+"&lt;coordinates>"+ vertexesCoord[vertex] +"&lt;/coordinates>"+ "<br>";
 		  	dkml = dkml +tab+tab+ "&lt;/vertex>"+ "<br>";
 		  }
-
+		  dkml = dkml + tab+ "&lt;/Vertexes>" + "<br>";
 		 
 		  var firstVertixId;
 		  var lastVertixId;
+		  var arrayOffSet = -1;
 
 		  for(var vertex in vertexesId){
 
 		  	  firstVertixId = vertexesId[vertex];
 		  	  
 		  	  if(lastVertixId>=0){
-		  	  	
-		  	  	  dkml = dkml +tab+ "&lt;/Vertexes>"+ "<br>";
 				  dkml = dkml +tab+ "&lt;Placemark>"+ "<br>";
 				  dkml = dkml +tab+tab+ "&lt;name>"+lastVertixId+"-"+firstVertixId+"&lt;/name>"+"<br>";
 				  dkml = dkml +tab+tab+ "&lt;weight>"+"1"+"&lt;/weight>"+"<br>";
 				  dkml = dkml +tab+tab+ "&lt;src>"+lastVertixId+"&lt;/src>"+"<br>";
 				  dkml = dkml +tab+tab+ "&lt;dest>"+firstVertixId+"&lt;/dest>"+"<br>";
 				  dkml = dkml +tab+tab+ "&lt;LineString>"+"<br>";
-				  dkml = dkml +tab+tab+tab+ "&lt;coordinates>"+"<br>";
+				  dkml = dkml +tab+tab+tab+ "&lt;coordinates>";
 				  for (var i = lastVertixId; i <= firstVertixId; i++) {
-				  	dkml = dkml +tab+tab+tab+tab+arrayOfCoord[someInt-i]+"<br>";	
+				  	if (i == firstVertixId) {
+				  		dkml = dkml+arrayOfCoord[i-arrayOffSet];
+				  	}
+				  	else{
+				  		dkml = dkml+arrayOfCoord[i-arrayOffSet]+" ";
+				  	}	
 				  };
-				  dkml = dkml +tab+tab+tab+ "&lt;/coordinates>"+"<br>";
+				  dkml = dkml +"&lt;/coordinates>"+"<br>";
 				  dkml = dkml +tab+tab+ "&lt;/LineString>"+"<br>";
 				  dkml = dkml +tab+ "&lt;/Placemark>"+ "<br>";
 			  }
+			  if (arrayOffSet == -1) {
+			  		arrayOffSet = vertexesId[vertex];
+			  };
 			  lastVertixId = vertexesId[vertex];
 			}
-		 dkml = dkml+"&lt;/Document>";	
+		 	
 
 		  //dkml = dkml.replace(/&/g, '&amp;').replace(/</g, '&lt;');
 		  return dkml;
